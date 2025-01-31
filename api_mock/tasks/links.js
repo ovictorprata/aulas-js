bebcc5c742ec56ee2162c6ffa4baa68efef49946
pratinha <victorsousaprata@gmail.com>
2025-01-31 10:51:10 -0300

    sobe ementa nova e reorganiza para ementa nova

--- api_mock/tasks/links.js
+++
@@ -1,7 +0,0 @@
-const raposas = [
-    'https://www.worcestermag.com/gcdn/presto/2022/12/01/NWKL/0c47fbe0-edc9-4e6b-b01b-e35096288fca-socks_the_fox.jpg',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNo8S7r_w_xs_I_AjcWJ5J5uCiieFLwbHAbA&s',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4lQNwX7ptm3bPkIFqM0F39tZ0ZVFYoqDL8w&s',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPR-CFklKlAx3Pc8Yb7Yy2JooKcK25weWNRA&s',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS46dQf65swxfQR_8duU9ONs9ypPpcIDBlBuw&s'
-]
